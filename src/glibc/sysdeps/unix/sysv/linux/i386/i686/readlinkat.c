0ab26ffffa26283f79e4e23006886df17304b11c
Alice W. <wenyaxuan0925@outlook.com>
2025-01-05 15:15:20 -0500

    update readlinkat glibc impl

--- src/glibc/sysdeps/unix/sysv/linux/i386/i686/readlinkat.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/i686/readlinkat.c
@@ -1,10 +1,28 @@
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <string.h>
+
+// ssize_t __GI_readlinkat (int __fd, const char *__file_name, char *__buf, size_t __len)
+// {
+//   return 0;
+// }
+
+// weak_alias(__GI_readlinkat, _readlinkat)
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
+#include <sysdep-cancel.h>
+#include <syscall-template.h>
 
-ssize_t __GI_readlinkat (int __fd, const char *__file_name, char *__buf, size_t __len)
+/* Read the contents of the symbolic link PATH into no more than
+   LEN bytes of BUF.  The contents are not null-terminated.
+   Returns the number of characters read, or -1 for errors.  */
+/*
+* Edit Note:
+* We implement both `readlink` and `readlinkat` in RawPOSIX, so changed the normal 
+*/
+ssize_t
+__libc_readlinkat (int fd, const char *path, char *buf, size_t len)
 {
-  return 0;
+  return MAKE_SYSCALL(166, "syscall|readlinkat",(uint64_t) fd, (uint64_t) path, (uint64_t)(uintptr_t) buf, (uint64_t) len, NOTUSED, NOTUSED);
 }
-
-weak_alias(__GI_readlinkat, _readlinkat)
+weak_alias(__libc_readlinkat, readlinkat)
