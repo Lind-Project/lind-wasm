786e0821eded2e6f6a7a7eb9a9024f1333f81a1e
robinyuan1002 <ry2361@nyu.edu>
2025-02-07 15:47:24 -0500

    delete Rawposix getifaddrs test case

--- src/RawPOSIX/src/tests/networking_tests.rs
+++ src/RawPOSIX/src/tests/networking_tests.rs
@@ -4269,33 +4269,6 @@ pub mod net_tests {
     }
 
     #[test]
-    pub fn ut_lind_net_getifaddrs() {
-        // acquiring a lock on TESTMUTEX prevents other tests from running concurrently,
-        // and also performs clean env setup
-        let _thelock = setup::lock_and_init();
-
-        let cage = interface::cagetable_getref(1);
-
-        // get the address
-        let mut buf = vec![0u8; 200];
-        let bufptr: *mut u8 = &mut buf[0];
-        // not enough space would cause EOPNOTSUPP error
-        assert_eq!(
-            cage.getifaddrs_syscall(bufptr, 1),
-            -(Errno::EOPNOTSUPP as i32)
-        );
-        assert_eq!(cage.getifaddrs_syscall(bufptr, 200), 0);
-        // split the address string into vector
-        let result = std::str::from_utf8(&buf).unwrap().replace("\0", "");
-        let addrs = result.trim().split("\n").collect::<Vec<&str>>();
-        // we should have some addresses returned
-        assert!(addrs.len() > 0);
-
-        assert_eq!(cage.exit_syscall(EXIT_SUCCESS), EXIT_SUCCESS);
-        lindrustfinalize();
-    }
-
-    #[test]
     pub fn ut_lind_net_dns_rootserver_ping() {
         //acquiring a lock on TESTMUTEX prevents other tests from running concurrently,
         // and also performs clean env setup
