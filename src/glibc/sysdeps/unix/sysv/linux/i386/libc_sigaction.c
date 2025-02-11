17dc54625ff3f6adff369bb3a604b31c728e957c
Qianxi Chen <qc815@nyu.edu>
2025-02-11 06:41:40 +0000

    clean up for glibc

--- src/glibc/sysdeps/unix/sysv/linux/i386/libc_sigaction.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/libc_sigaction.c
@@ -83,12 +83,15 @@ extern void restore (void) {
 
 // RESTORE (restore, __NR_sigreturn)
 
+// entry point of epoch callback in glibc, invoked by wasmtime
 __attribute__((export_name("signal_callback")))
 void signal_callback(__sighandler_t callback, int signal) {
+  // directly call into user's custom signal handler
   if(callback != 0)
     callback(signal);
 }
 
+// rawposix sigaction struct
 struct rawposix_sigaction {
   __sighandler_t handler;
   unsigned long long sa_mask;
@@ -98,19 +101,24 @@ struct rawposix_sigaction {
 int
 __libc_sigaction (int sig, const struct sigaction *act, struct sigaction *oact)
 {
-  struct rawposix_sigaction rawposix_act;
-  struct rawposix_sigaction rawposix_oact;
-  rawposix_act.handler = act->sa_handler;
-  rawposix_act.sa_mask = act->sa_mask.__val[0];
-  rawposix_act.sa_flags = act->sa_flags;
-  // for(int i = 0; i < sizeof(act->sa_mask) / sizeof(unsigned long int); ++i)
-  //   printf("%d ", act->sa_mask.__val[i]);
-  // printf("\n");
-  int retval = MAKE_SYSCALL(147, "syscall|sigaction", (uint64_t) sig, (uint64_t) &rawposix_act, (uint64_t) &rawposix_oact, NOTUSED, NOTUSED, NOTUSED);
+  // we do the manual translation between glibc sigaction struct and rawposix sigaction struct here
+  struct rawposix_sigaction rawposix_act, rawposix_oact;
+  // check for NULL pointer
+  if (act)
+  {
+    rawposix_act.handler = act->sa_handler;
+    rawposix_act.sa_mask = act->sa_mask.__val[0];
+    rawposix_act.sa_flags = act->sa_flags;
+  }
+  int retval = MAKE_SYSCALL(147, "syscall|sigaction", (uint64_t) sig, (uint64_t) (act ? &rawposix_act : NULL), (uint64_t) (oact ? &rawposix_oact : NULL), NOTUSED, NOTUSED, NOTUSED);
 
-  oact->sa_handler = rawposix_oact.handler;
-  oact->sa_mask.__val[0] = rawposix_oact.sa_mask;
-  oact->sa_flags = rawposix_oact.sa_flags;
+  // check for NULL pointer
+  if (oact)
+  {
+    oact->sa_handler = rawposix_oact.handler;
+    oact->sa_mask.__val[0] = rawposix_oact.sa_mask;
+    oact->sa_flags = rawposix_oact.sa_flags;
+  }
 
   return retval;
 }
--- src/glibc/sysdeps/unix/sysv/linux/sigprocmask.c
+++ src/glibc/sysdeps/unix/sysv/linux/sigprocmask.c
@@ -18,16 +18,20 @@
 #include <signal.h>
 #include <pthreadP.h>              /* SIGCANCEL, SIGSETXID */
 #include <syscall-template.h>
-#include <stdio.h>
 
 /* Get and/or change the set of blocked signals.  */
 int
 __sigprocmask (int how, const sigset_t *set, sigset_t *oset)
 {
+   // we do the manual translation between glibc sigset type and rawposix sigset type here
    unsigned long long rawposix_set, rawposix_oset;
-   rawposix_set = set->__val[0];
-   int retval = MAKE_SYSCALL(149, "syscall|sigprocmask", (uint64_t) how, (uint64_t) &rawposix_set, (uint64_t) &rawposix_oset, NOTUSED, NOTUSED, NOTUSED);
-   oset->__val[0] = (unsigned long int) rawposix_oset;
+   // check for NULL pointer
+   if (set)
+      rawposix_set = set->__val[0];
+   int retval = MAKE_SYSCALL(149, "syscall|sigprocmask", (uint64_t) how, (uint64_t) (set ? &rawposix_set : NULL), (uint64_t) (oset ? &rawposix_oset : NULL), NOTUSED, NOTUSED, NOTUSED);
+   // check for NULL pointer
+   if (oset)
+      oset->__val[0] = (unsigned long int) rawposix_oset;
    return retval;
 }
 libc_hidden_def (__sigprocmask)
