fd4238806cc2f0db0b68172342497688460c3910
Qianxi Chen <qc815@nyu.edu>
2025-02-11 07:44:45 +0000

    added signal tests

---
+++ tests/unit-tests/signal_tests/non-deterministic/signal_SIGCHLD.c
@@ -0,0 +1,48 @@
+// signal test for SIGCHLD signal when child exits
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <setjmp.h>
+
+// Custom signal handler
+void handle_signal(int signal) {
+    printf("Caught signal %d\n", signal);
+}
+
+int main() {
+    printf("main starts!\n");
+    struct sigaction sa;
+
+    sa.sa_handler = handle_signal;
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    int pid = fork();
+    if(pid == 0)
+    {
+        printf("child ready to kill\n");
+        kill(getppid(), SIGUSR1);
+        printf("child done kill\n");
+    }
+    else
+    {
+        while (1) {
+            printf("parent in loop, pid=%d\n", getpid());
+            sleep(1);
+        }
+        printf("parent outside loop (should not reach)\n");
+    }
+
+    return 0;
+}
