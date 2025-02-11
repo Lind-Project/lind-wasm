b0419970e03ddbb09a9c75c8d944c689b5d157a5
Alice W. <wenyaxuan0925@outlook.com>
2025-02-11 13:16:54 -0500

    Add posix-ish syscall num on glibc

---
+++ src/glibc/sysdeps/unix/sysv/linux/lind_syscall_num.h
@@ -0,0 +1,10 @@
+#ifndef LIND_SYSCALL_NUM_H
+#define LIND_SYSCALL_NUM_H
+
+/* System call numbers for Lind implementation */
+#define ACCESS_SYSCALL 21
+#define UNLINK_SYSCALL 87
+#define LINK_SYSCALL 86
+#define RENAME_SYSCALL 82
+
+#endif /* LIND_SYSCALL_NUM_H */
