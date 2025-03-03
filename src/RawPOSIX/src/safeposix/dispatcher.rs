53e9d823f68c04d2c94dc9a228c67529b074f369
ChinmayShringi <chinmayshringi4@gmail.com>
2025-03-03 16:55:09 +0000

    fix: addr type

--- src/RawPOSIX/src/safeposix/dispatcher.rs
+++ src/RawPOSIX/src/safeposix/dispatcher.rs
@@ -277,7 +277,7 @@ pub fn lind_syscall_api(
 
         MPROTECT_SYSCALL => {
             let cage = interface::cagetable_getref(cageid);
-            let addr = arg1;
+            let addr = arg1 as *mut u8;
             let len = arg2 as usize;
             let prot = arg3 as i32;
             
