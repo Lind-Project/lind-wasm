b1e72b1dd8c27f83434cfd72b8ff64a4feb70f7b
ChinmayShringi <chinmayshringi4@gmail.com>
2025-03-07 16:38:56 +0000

    feat: updated alias

--- src/glibc/sysdeps/unix/sysv/linux/mprotect.c
+++ src/glibc/sysdeps/unix/sysv/linux/mprotect.c
@@ -10,10 +10,5 @@ __GI___mprotect (void *addr, size_t len, int prot)
   return MAKE_SYSCALL(177, "syscall|mprotect", (uint64_t)(uintptr_t) addr, (uint64_t) len, (uint64_t) prot, NOTUSED, NOTUSED, NOTUSED);
 }
 
-int
-mprotect (void *addr, size_t len, int prot)
-{
-  return MAKE_SYSCALL(177, "syscall|mprotect", (uint64_t)(uintptr_t) addr, (uint64_t) len, (uint64_t) prot, NOTUSED, NOTUSED, NOTUSED);
-}
-
 weak_alias(__GI___mprotect, __mprotect)
+weak_alias(__GI___mprotect, mprotect)
