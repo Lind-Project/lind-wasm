9c38bdb35082db60bc74a58609ece85ad65e03e1
Alice W. <wenyaxuan0925@outlook.com>
2025-03-24 09:31:24 -0400

    Syscall number unification in glibc

--- src/glibc/sysdeps/unix/sysv/linux/fcntl.c
+++ src/glibc/sysdeps/unix/sysv/linux/fcntl.c
@@ -21,94 +21,86 @@
 #include <errno.h>
 #include <sysdep-cancel.h>
 #include <syscall-template.h>
-
+#include <lind_syscall_num.h>
 #ifndef __OFF_T_MATCHES_OFF64_T
-
 # ifndef FCNTL_ADJUST_CMD
 #  define FCNTL_ADJUST_CMD(__cmd) __cmd
 # endif
-
 int
 __libc_fcntl (int fd, int cmd, ...)
 {
   va_list ap;
   void *arg;
-
   va_start (ap, cmd);
   arg = va_arg (ap, void *);
   va_end (ap);
-
   cmd = FCNTL_ADJUST_CMD (cmd);
-
   switch (cmd)
     {
       case F_SETLKW:
       case F_SETLKW64:
-		return MAKE_SYSCALL(28, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) arg, NOTUSED, NOTUSED, NOTUSED);
+                return MAKE_SYSCALL(FCNTL_SYSCALL, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) arg, NOTUSED, NOTUSED, NOTUSED);
       case F_OFD_SETLKW:
-	{
-	  struct flock *flk = (struct flock *) arg;
-	  struct flock64 flk64 =
-	  {
-	    .l_type = flk->l_type,
-	    .l_whence = flk->l_whence,
-	    .l_start = flk->l_start,
-	    .l_len = flk->l_len,
-	    .l_pid = flk->l_pid
-	  };
-		return MAKE_SYSCALL(28, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) &flk64, NOTUSED, NOTUSED, NOTUSED);
-	}
+        {
+          struct flock *flk = (struct flock *) arg;
+          struct flock64 flk64 =
+          {
+            .l_type = flk->l_type,
+            .l_whence = flk->l_whence,
+            .l_start = flk->l_start,
+            .l_len = flk->l_len,
+            .l_pid = flk->l_pid
+          };
+                return MAKE_SYSCALL(FCNTL_SYSCALL, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) &flk64, NOTUSED, NOTUSED, NOTUSED);
+        }
       case F_OFD_GETLK:
       case F_OFD_SETLK:
-	{
-	  struct flock *flk = (struct flock *) arg;
-	  struct flock64 flk64 =
-	  {
-	    .l_type = flk->l_type,
-	    .l_whence = flk->l_whence,
-	    .l_start = flk->l_start,
-	    .l_len = flk->l_len,
-	    .l_pid = flk->l_pid
-	  };
-	  int ret = MAKE_SYSCALL(28, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) &flk64, NOTUSED, NOTUSED, NOTUSED);
-	  if (ret == -1)
-	    return -1;
-	  if ((off_t) flk64.l_start != flk64.l_start
-	      || (off_t) flk64.l_len != flk64.l_len)
-	    {
-	      __set_errno (EOVERFLOW);
-	      return -1;
-	    }
-	  flk->l_type = flk64.l_type;
-	  flk->l_whence = flk64.l_whence;
-	  flk->l_start = flk64.l_start;
-	  flk->l_len = flk64.l_len;
-	  flk->l_pid = flk64.l_pid;
-	  return ret;
-	}
+        {
+          struct flock *flk = (struct flock *) arg;
+          struct flock64 flk64 =
+          {
+            .l_type = flk->l_type,
+            .l_whence = flk->l_whence,
+            .l_start = flk->l_start,
+            .l_len = flk->l_len,
+            .l_pid = flk->l_pid
+          };
+          int ret = MAKE_SYSCALL(FCNTL_SYSCALL, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, (uint64_t) &flk64, NOTUSED, NOTUSED, NOTUSED);
+          if (ret == -1)
+            return -1;
+if ((off_t) flk64.l_start != flk64.l_start
+              || (off_t) flk64.l_len != flk64.l_len)
+            {
+              __set_errno (EOVERFLOW);
+              return -1;
+            }
+          flk->l_type = flk64.l_type;
+          flk->l_whence = flk64.l_whence;
+          flk->l_start = flk64.l_start;
+          flk->l_len = flk64.l_len;
+          flk->l_pid = flk64.l_pid;
+          return ret;
+        }
       /* Since only F_SETLKW{64}/F_OLD_SETLK are cancellation entrypoints and
-	 only OFD locks require LFS handling, all others flags are handled
-	 unmodified by calling __NR_fcntl64.  */
+         only OFD locks require LFS handling, all others flags are handled
+         unmodified by calling __NR_fcntl64.  */
       default:
         return __fcntl64_nocancel_adjusted (fd, cmd, arg);
     }
 }
 libc_hidden_def (__libc_fcntl)
-
 weak_alias (__libc_fcntl, __fcntl)
 libc_hidden_weak (__fcntl)
-
 # include <shlib-compat.h>
 # if SHLIB_COMPAT(libc, GLIBC_2_0, GLIBC_2_28)
 int
 __old_libc_fcntl64 (int fd, int cmd, ...)
 {
-	return MAKE_SYSCALL(28, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
+        return MAKE_SYSCALL(FCNTL_SYSCALL, "syscall|fcntl", (uint64_t) fd, (uint64_t) cmd, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }
 compat_symbol (libc, __old_libc_fcntl64, fcntl, GLIBC_2_0);
 versioned_symbol (libc, __libc_fcntl, fcntl, GLIBC_2_28);
 # else
 weak_alias (__libc_fcntl, fcntl)
 # endif
-
 #endif /* __OFF_T_MATCHES_OFF64_T  */
