7b28e612e3b41edf19280e3c785163ef7e55ae59
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-15 05:39:05 +0000

    Fix: Revert unintended changes in dup.c and ensure correct syscall usage via DUP_SYSCALL macro

--- src/glibc/sysdeps/unix/sysv/linux/dup.c
+++ src/glibc/sysdeps/unix/sysv/linux/dup.c
@@ -1,21 +1,18 @@
-/* Linux dup syscall implementation.
-   Copyright (C) 2017-2024 Free Software Foundation, Inc.
+/* Duplicate a file descriptor.  Linux version.
+   Copyright (C) 2011-2024 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+	@@ -9,26 +9,24 @@
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library. If not, see
-   <https://www.gnu.org/licenses/>.  
-*/
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
 
 #include <unistd.h>
 #include <sysdep.h>
@@ -28,5 +25,5 @@ __dup (int fd)
 {
    return MAKE_SYSCALL(DUP_SYSCALL, "syscall|dup", (uint64_t) fd, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }
-libc_hidden_def (dup)
-
+libc_hidden_def (dup);
+weak_alias (__dup, dup);
