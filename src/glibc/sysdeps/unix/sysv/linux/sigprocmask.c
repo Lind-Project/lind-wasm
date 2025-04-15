cca19e67a69244fd68a78615c600813726fef072
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-15 19:41:21 +0000

    Fix: Replace syscall 149 with SYSCALL_SIGPROCMASK and clean up aliases in sigprocmask.c

--- src/glibc/sysdeps/unix/sysv/linux/sigprocmask.c
+++ src/glibc/sysdeps/unix/sysv/linux/sigprocmask.c
@@ -29,14 +29,14 @@ __sigprocmask (int how, const sigset_t *set, sigset_t *oset)
     rawposix_set = set->__val[0];
 
   int retval = MAKE_SYSCALL (
-      SYSCALL_SIGPROCMASK, "syscall|sigprocmask", (uint64_t) how,
-      (uint64_t) (set ? &rawposix_set : NULL),
-      (uint64_t) (oset ? &rawposix_oset : NULL), NOTUSED, NOTUSED, NOTUSED);
+    SIGPROCMASK_SYSCALL, "syscall|sigprocmask", (uint64_t) how,
+    (uint64_t) (set ? &rawposix_set : NULL),
+    (uint64_t) (oset ? &rawposix_oset : NULL), NOTUSED, NOTUSED, NOTUSED);
 
   if (oset)
     oset->__val[0] = (unsigned long int) rawposix_oset;
 
   return retval;
 }
-libc_hidden_def (__sigprocmask) weak_alias (__sigprocmask, sigprocmask)
+libc_hidden_def (__sigprocmask) 
 weak_alias (__sigprocmask, sigprocmask)
