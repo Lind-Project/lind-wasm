0ac3635040c4186df994e2b86457b46fab10bcdb
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-15 05:12:58 +0000

    Restore unintentionally deleted blank lines in syscall-template.h

--- src/glibc/sysdeps/unix/syscall-template.h
+++ src/glibc/sysdeps/unix/syscall-template.h
@@ -2,8 +2,10 @@
 #include <stdint.h> // For uint64_t
 #include <unistd.h>
 #include <lind_syscall.h>
+
 // Define NOTUSED for unused arguments
 #define NOTUSED 0xdeadbeefdeadbeefULL
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 #define WARPPED_SYSCALL 0
@@ -73,3 +75,10 @@ lind_syscall(syscallnum, (unsigned long long)(callname), (unsigned long long)(NO
     lind_syscall(syscallnum, (unsigned long long)(callname), (unsigned long long)(arg1), (unsigned long long)(arg2), (unsigned long long)(arg3), \
                  (unsigned long long)(arg4), (unsigned long long)(arg5), (unsigned long long)(arg6))
 >>>>>>> 9c38bdb3 (Syscall number unification in glibc)
+=======
+
+#define MAKE_SYSCALL(syscallnum, callname, arg1, arg2, arg3, arg4, arg5, arg6) \
+    lind_syscall(syscallnum, (unsigned long long)(callname), (unsigned long long)(arg1), (unsigned long long)(arg2), (unsigned long long)(arg3), \
+                 (unsigned long long)(arg4), (unsigned long long)(arg5), (unsigned long long)(arg6))
+
+>>>>>>> 1b546269 (Restore unintentionally deleted blank lines in syscall-template.h)
