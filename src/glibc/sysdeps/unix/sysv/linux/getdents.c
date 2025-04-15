23b04ca4fb6c3ea1a48a90555452c1663eaadec6
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-15 06:57:59 +0000

    Update getdents.c to use GETDENTS_SYSCALL macro and restore formatting

--- src/glibc/sysdeps/unix/sysv/linux/getdents.c
+++ src/glibc/sysdeps/unix/sysv/linux/getdents.c
@@ -19,19 +19,26 @@
 #include <dirent.h>
 #include <syscall-template.h>
 #include <lind_syscall_num.h>
+
 #if !_DIRENT_MATCHES_DIRENT64
+
 # include <unistd.h>
 # include <string.h>
 # include <errno.h>
+
+
 # ifndef DIRENT_SET_DP_INO
 #  define DIRENT_SET_DP_INO(dp, value) (dp)->d_ino = (value)
 # endif
+
 /* Pack the dirent64 struct down into 32-bit offset/inode fields, and
    ensure that no overflow occurs.  */
 ssize_t
 __getdents (int fd, void *buf0, size_t nbytes)
 {
-        return MAKE_SYSCALL(GETDENTS_SYSCALL, "syscall|getdents", (uint64_t) fd, (uint64_t) buf0, (uint64_t) nbytes, NOTUSED, NOTUSED, NOTUSED);
+   return MAKE_SYSCALL(GETDENTS_SYSCALL, "syscall|getdents", (uint64_t) fd, (uint64_t) buf0, (uint64_t) nbytes, NOTUSED, NOTUSED, NOTUSED);
 }
+
 # undef DIRENT_SET_DP_INO
-#endif /* *DIRENT*MATCHES_DIRENT64  */
+
+#endif /* _DIRENT_MATCHES_DIRENT64  */
