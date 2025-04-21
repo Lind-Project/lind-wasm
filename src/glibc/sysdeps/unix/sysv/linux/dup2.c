f2aca47ce6c1881fe60fbe12090f4d368a4c517e
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-21 01:39:25 -0400

    revert to minimise git diff

--- src/glibc/sysdeps/unix/sysv/linux/dup2.c
+++ src/glibc/sysdeps/unix/sysv/linux/dup2.c
@@ -1,5 +1,20 @@
-/* Duplicate a file descriptor.  Linux version.  */
-/* Copyright (C) 2011–2024 Free Software Foundation, Inc. */
+/* Duplicate a file descriptor.  Linux version.
+   Copyright (C) 2011-2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -7,15 +22,10 @@
 #include <syscall-template.h>
 #include <lind_syscall_num.h>
 
-/* Duplicate FD to FD2, closing the old FD2 and making FD2 be
-   open the same file as FD is.  Return FD2 or -1.  */
 int
 __dup2 (int fd, int fd2)
 {
-  return MAKE_SYSCALL(DUP2_SYSCALL, "syscall|dup2",
-                      (uint64_t) fd, (uint64_t) fd2,
-                      NOTUSED, NOTUSED, NOTUSED, NOTUSED);
+   return MAKE_SYSCALL(DUP2_SYSCALL, "syscall|dup2", (uint64_t) fd, (uint64_t) fd2, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }
-
 libc_hidden_def (__dup2)
 weak_alias (__dup2, dup2)
