e1779ac28732b38fa7ecb13f94db1351ad912c91
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-04-21 01:29:00 -0400

    revert to minimise git diff

--- src/glibc/sysdeps/unix/sysv/linux/dup.c
+++ src/glibc/sysdeps/unix/sysv/linux/dup.c
@@ -3,7 +3,9 @@
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
-	@@ -9,26 +9,24 @@
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,16 +16,21 @@
    License along with the GNU C Library.  If not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <unistd.h>
-#include <sysdep.h>
-#include <syscall-template.h>
-#include <lind_syscall_num.h>
-
-/* Duplicate FD to a new file descriptor. */
-int
-__dup (int fd)
-{
-   return MAKE_SYSCALL(DUP_SYSCALL, "syscall|dup", (uint64_t) fd, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
-}
-libc_hidden_def (dup);
-weak_alias (__dup, dup);
+   #include <fcntl.h>
+   #include <unistd.h>
+   #include <sysdep.h>
+   #include <syscall-template.h>
+   #include <lind_syscall_num.h>
+   
+   /* Duplicate FD to FD2, closing the old FD2 and making FD2 be
+      open the same file as FD is.  Return FD2 or -1.  */
+   int
+   __dup (int fd)
+   {
+      return MAKE_SYSCALL(DUP_SYSCALL, "syscall|dup", (uint64_t) fd, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
+   }
+   
+   int dup (int fd) {
+      return MAKE_SYSCALL(DUP_SYSCALL, "syscall|dup", (uint64_t) fd, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
+   }
+   
