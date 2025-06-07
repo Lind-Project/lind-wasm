7ad4dc1b062909dd408b289942807a3a68224607
xhanghe <mikaelhi1213@gmail.com>
2025-06-07 21:40:30 +0000

    additional space change

--- src/glibc/sysdeps/unix/sysv/linux/connect.c
+++ src/glibc/sysdeps/unix/sysv/linux/connect.c
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
-   
+
 #include <sys/socket.h>
 #include <sysdep-cancel.h>
 #include <socketcall.h>
