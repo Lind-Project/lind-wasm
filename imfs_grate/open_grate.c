20306c7fab90752965a8eb2f5696db6cd954b3b4
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2025-09-01 08:32:05 -0400

    [imfs_grate] working version. nicer logs

--- imfs_grate/open_grate.c
+++ imfs_grate/open_grate.c
@@ -12,7 +12,91 @@
 
 #include <imfs.h>
 
+static inline void sys_log_args(const char *name, 
+				uint64_t arg1,
+				uint64_t arg2,
+				uint64_t arg3,
+				uint64_t arg4,
+				uint64_t arg5,
+				uint64_t arg6,
+				int ret) {
+	char buf[512];
+	size_t pos = 0;
+
+	pos += snprintf(buf + pos, sizeof(buf) - pos, "%s (", name);
+
+	uint64_t args[6] = { arg1, arg2, arg3, arg4, arg5, arg6 };
+	int first = 1;
+
+	for (int i=0; i < 6; i++) {
+		if(args[i] == 0xdeadbeefdeadbeefULL) continue;
+
+		if(!first) pos += snprintf(buf + pos, sizeof(buf) - pos, ", ");
+
+		pos += snprintf(buf + pos, sizeof(buf) - pos, "%llu", args[i]);
+		first = 0;
+	}
+
+	snprintf(buf + pos, sizeof(buf) - pos, ") = %d\n", ret);
+
+	fprintf(stderr, "%s", buf);
+
+}
+
+#define SYS_LOG(name, ret) \
+	sys_log_args((name), arg1, arg2, arg3, arg4, arg5, arg6, (ret))	
+
 FILE *fp;
+
+char *read_full_file(char *path, size_t *out_size) {
+	FILE *fp = fopen(path, "rb");
+
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+	rewind(fp);
+
+	char *buf = malloc(size);
+
+	size_t read = fread(buf, 1, size, fp);
+	fclose(fp);
+	*out_size = (size_t) size;
+
+	return buf;
+}
+
+void dump_file(char *path, char *actual_path) {
+	char split_path[4096];
+	strcpy(split_path, path);
+
+	for(char *p = split_path + 1; *p;p++) {
+		if(*p == '/') {
+			*p = '\0';
+			int ret = mkdir(split_path, 0755);
+			*p = '/';
+		}
+	}
+
+	int fd = open(actual_path, O_CREAT | O_WRONLY | O_APPEND, 0777);
+	int ifd = imfs_open(0, path, O_RDONLY, 0);
+
+	size_t nread;
+	char buf[1024];
+
+	while(1) {
+		char buf[1024];
+		size_t nread = imfs_new_read(0, ifd, buf, 1024);
+
+		if(nread <= 0) {
+			break;
+		}
+
+		write(fd, buf, nread);
+	}
+
+	close(fd);
+	imfs_close(0, ifd);
+}
+
 
 void load_file(char *path) {
 	FILE *fp = fopen("preloads.log", "a");
@@ -31,35 +115,15 @@ void load_file(char *path) {
 		}
 	}
 
-
 	int imfs_fd = imfs_open(0, path, O_CREAT | O_WRONLY, 0777);
 	fprintf(fp, "[load_file] created file: %s\n", path);
 
-	int fd = open(path, O_RDONLY, 0);
-	if(fd < 0) {
-		perror("open error");
-		return;
-	}
+	size_t size;
+	char *data = read_full_file(path, &size);
 
-	size_t nread;
-	while(1) {
-		char *buffer = malloc(1024);
+	imfs_write(0, imfs_fd, data, size);
+	free(data);
 
-		nread = read(fd, buffer, 1024);
-		
-		if(nread == 0) {
-			free(buffer);
-			break;
-		}
-		
-		fprintf(fp, "nread=%d   ", nread);
-		imfs_write(0, imfs_fd, buffer, nread);
-
-		free(buffer);
-	}
-
-	fclose(fp);
-	close(fd);
 	imfs_close(0, imfs_fd);
 }
 
@@ -116,22 +180,28 @@ int open_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2,
 	int ifd = imfs_open(cageid, pathname, arg2, arg3); 
 
 	if(ifd < 0) {
-		fprintf(stderr,"errno=%d\n", errno);
-		
 		FILE *failed_opens = fopen("failed_opens.log", "a");
 		fprintf(failed_opens, "PATH=%s | RET=%d\n", pathname, errno);
 		fclose(failed_opens);
 		
 		perror("imfs open failed.");
 	}
-	
-	fprintf(stderr, "\nOPEN (cageid=%llu, pathname=%s, flags=%llu, mode=%llu )  = %d\n\n", cageid, pathname, arg2, arg3, ifd);
+
+	SYS_LOG("OPEN", ifd);
+
+	free(pathname);
 	return ifd;
+}
+
+int fcntl_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2, uint64_t arg2cage, uint64_t arg3, uint64_t arg3cage, uint64_t arg4, uint64_t arg4cage, uint64_t arg5, uint64_t arg5cage, uint64_t arg6, uint64_t arg6cage){
+	int ret = imfs_fcntl(cageid, arg1, arg2, arg3);
+	SYS_LOG("FCNTL", ret);
+	return ret;
 }
 
 int close_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2, uint64_t arg2cage, uint64_t arg3, uint64_t arg3cage, uint64_t arg4, uint64_t arg4cage, uint64_t arg5, uint64_t arg5cage, uint64_t arg6, uint64_t arg6cage){
 	int ret = imfs_close(cageid, arg1);
-	fprintf(stderr, "CLOSE (cageid=%llu, fd=%llu)\n", cageid, arg1);
+	SYS_LOG("CLOSE", ret);	
 	return ret;
 }
 
@@ -143,8 +213,9 @@ off_t lseek_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t ar
 	int whence = (int) arg3;
 	
 	off_t ret = imfs_lseek(cageid, fd, offset, whence);
-	
-	fprintf(stderr, "\nLSEEK (cage_id=%llu, fd=%llu, offset=%llu, whence=%llu) =%ld\n\n", cageid, arg1, arg2, arg3, ret);
+
+	SYS_LOG("LSEEK", ret);
+
 	return ret;
 }
 
@@ -165,7 +236,7 @@ int read_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2,
 
 	ret = imfs_read(cageid, arg1, buf, count);
 
-	fprintf(stderr, "READ: imfs_read done. ARG2=%llu\n", arg2);
+	// Do not call cp_data if target buffer is NULL.
 	if(arg2 != 0) {
 		cp_data_between_cages(
 			thiscage, 
@@ -177,11 +248,9 @@ int read_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2,
 			count,
 			0 // 1
 		);
-	} else {
-		fprintf(stderr, "READ: buffer is NULL\n");
 	}
-
-	fprintf(stderr, "\nREAD (cageid=%llu, fd=%llu, buf=[], count=%d) =%d\n\n", cageid, arg1, count, ret);
+ 
+	SYS_LOG("READ", ret);
 
 	free(buf);
 
@@ -200,7 +269,16 @@ int write_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2
 		exit(1);
 	}
 
-	cp_data_between_cages(thiscage, arg2cage, arg2, arg2cage, (uint64_t) buffer, thiscage, count, 0);
+	cp_data_between_cages(
+		thiscage,
+		arg2cage,
+		arg2,
+		arg2cage,
+		(uint64_t) buffer,
+		thiscage,
+		count,
+		0
+	);
 
 	if(arg1 < 3) {
 		int hfd = open("host_write", O_WRONLY | O_APPEND, 0);
@@ -208,10 +286,10 @@ int write_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage, uint64_t arg2
 		close(hfd);
 		return count;
 	}
-
-	ret = imfs_write(cageid, arg1, buffer, count);
-
-	fprintf(stderr, "\nWRITE(cageid=%llu, fd=%llu, buffer=%s, count=%llu) =%d\n\n", cageid, arg1, buffer, arg3, ret);
+	
+	ret = imfs_new_write(cageid, arg1, buffer, count);
+	SYS_LOG("WRITE", ret);
+	free(buffer);
 
 	return ret;
 }
@@ -223,7 +301,6 @@ void preloads() {
 		return;
 	}
 
-	fprintf(stderr, "[load_file] ENV: %s\n", env);
 	char *list = strdup(env);
 	if(!list) {
 		return;
@@ -243,8 +320,6 @@ void preloads() {
 			continue;
 		}
 	
-		fprintf(fp, "st_mode: %d REG: %d DIR: %d\n", st.st_mode, S_ISREG(st.st_mode), S_ISDIR(st.st_mode));
-
 		if(strlen(line) > 0) {
 			if (S_ISREG(st.st_mode))
 				load_file(line);
@@ -274,10 +349,8 @@ int main(int argc, char *argv[]) {
     // - the second is when there is still at least one grate in the subsequent command line input. 
     // In the second case, we fork & exec the new grate and let the new grate handle the subsequent process.
 
-
     for (int i = 1; i < (argc < 3 ? argc : 3); i++) {
         pid_t pid = fork();
-	fprintf(stderr, "\n\ni=%d pid=%d\n", i, pid);
         if (pid < 0) {
             perror("fork failed");
             exit(EXIT_FAILURE);
@@ -286,25 +359,27 @@ int main(int argc, char *argv[]) {
                 int cageid = getpid();
 		int ret;
 
+		// Sleeping allows for parent proc to preload files into memory.
 		fprintf(stderr, "Sleeping for 3\n");
 		sleep(3);
-
-		fprintf(stderr, "Starting Registrations\n");
-		
+	
+		// OPEN	
 		ret = register_handler(cageid, 2, 0, grateid);
-                fprintf(stderr, "open register ret=%d\n", ret);
-	    
+	   
+		// LSEEK 
 		ret = register_handler(cageid, 8, 1, grateid);
-		fprintf(stderr, "lseek register ret=%d\n", ret);
-	    
+	   
+		// READ 
 		ret = register_handler(cageid, 0, 2, grateid);
-		fprintf(stderr, "read register ret=%d\n", ret);
 	    
+		// WRITE
 	    	ret = register_handler(cageid, 1, 3, grateid);
-		fprintf(stderr, "write register ret=%d\n", ret);
 
+		// CLOSE
 		ret = register_handler(cageid, 3, 4, grateid);
-		fprintf(stderr, "close register ret=%d\n", ret);
+	   
+		// FCNTL 
+		ret = register_handler(cageid, 72, 5, grateid);
 	    }
 
 	    fprintf(stderr,"\n\n---Execing argv[i]=%s---\n\n", argv[i]);
@@ -314,10 +389,7 @@ int main(int argc, char *argv[]) {
                 exit(EXIT_FAILURE);
             }
         } else {
-  	    printf("Enter sandman\n");
-    	    printf("\nargc: %d argv[1]:%s argv[2]=%s\n\n", argc, argv[1], argv[2]);
     	    imfs_init();
-    	    fprintf(stderr, "\nimfs_init() called.\n\n");
     	    preloads();
 	}
     }
@@ -333,6 +405,8 @@ int main(int argc, char *argv[]) {
 		perror("[Grate] [Wait]");
 	}
     }
-   
+  
+    dump_file("/tccgrateout", "tcc_grate_out");
+
     return 0;
 }
