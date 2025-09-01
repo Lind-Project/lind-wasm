4e41b027451c6dfcafb91aec669d45f5758a13d2
Nicholas Renner <nicholassrenner@gmail.com>
2025-09-01 13:06:37 -0400

    update tomls

--- src/wasmtime/crates/wasmtime/src/runtime/instance.rs
+++ src/wasmtime/crates/wasmtime/src/runtime/instance.rs
@@ -13,6 +13,7 @@ use crate::{
 use alloc::sync::Arc;
 use core::ptr::NonNull;
 use rawposix::safeposix::dispatcher::lind_syscall_api;
+use cage::memory::{init_vmmap, fork_vmmap};
 use sysdefs::constants::fs_const::{
     MAP_ANONYMOUS, MAP_FIXED, MAP_PRIVATE, PAGESHIFT, PROT_READ, PROT_WRITE,
 };
