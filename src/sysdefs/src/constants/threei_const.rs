40412ad086bf3258bacf24cd082538554de47057
Sankalp Ramesh <ssannkkallpp@gmail.com>
2025-09-08 18:57:56 +0000

    Resolved more threei build dependency issues

---
+++ src/sysdefs/src/constants/threei_const.rs
@@ -0,0 +1,6 @@
+/// Need to replace by either linux number or purposed more appropriate error num
+/// Deregister/matchall should be large enough to avoid confusion
+pub const THREEI_DEREGISTER: u64 = 500;
+pub const THREEI_MATCHALL: u64 = 501;
+pub const ELINDAPIABORTED: u64 = 0xFFFFFFFF;
+pub const ELINDESRCH: u64 = 0xFFFFFFFF;
