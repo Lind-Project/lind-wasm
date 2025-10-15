9dc20fc7bee6bbcaf5563f72a70e292847dc62b6
ssannkkallpp <ssannkkallpp@gmail.com>
2025-10-15 16:03:51 +0000

    Added macro for writev.c

--- src/glibc/sysdeps/unix/sysv/linux/writev.c
+++ src/glibc/sysdeps/unix/sysv/linux/writev.c
@@ -1,20 +1,20 @@
 /* Linux writev syscall implementation.
-   Copyright (C) 2017-2024 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+Copyright (C) 2017-2024 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <https://www.gnu.org/licenses/>.  */
+You should have received a copy of the GNU Lesser General Public
+License along with the GNU C Library; if not, see
+<https://www.gnu.org/licenses/>.  */
 
 #include <unistd.h>
 #include <sys/uio.h>
@@ -26,7 +26,13 @@ ssize_t
 __writev (int fd, const struct iovec *iov, int iovcnt)
 {
   // Dennis Edit
-  return MAKE_SYSCALL(WRITEV_SYSCALL, "syscall|writev", (uint64_t) fd, (uint64_t)(uintptr_t) iov, (uint64_t) iovcnt, NOTUSED, NOTUSED, NOTUSED);
+  struct iovec *host_iov = malloc(iovcnt * sizeof(struct iovec));
+  for (int i = 0; i < iovcnt; ++i) {
+    host_iov[i].iov_base = TRANSLATE_GUEST_POINTER_TO_HOST(user_iov[i].iov_base);
+    host_iov[i].iov_len  = user_iov[i].iov_len;
+  }
+
+  return MAKE_SYSCALL(WRITEV_SYSCALL, "syscall|writev", (uint64_t) fd, (uint64_t)(uintptr_t) host_iov, (uint64_t) iovcnt, NOTUSED, NOTUSED, NOTUSED);
 }
 libc_hidden_def (__writev)
 weak_alias (__writev, writev)
