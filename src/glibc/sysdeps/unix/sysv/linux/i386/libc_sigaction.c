8d797d7edc3773f43a659963850fb225a4fcc7a9
ssannkkallpp <ssannkkallpp@gmail.com>
2025-10-17 14:44:36 -0400

    Minor indentation fix

--- src/glibc/sysdeps/unix/sysv/linux/i386/libc_sigaction.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/libc_sigaction.c
@@ -24,51 +24,62 @@ License along with the GNU C Library; if not, see
 
 #define SA_RESTORER 0x04000000
 
-extern void restore_rt (void) {
-
-}
-extern void restore (void) {
-
+extern void
+restore_rt (void)
+{
 }
 
+extern void
+restore (void)
+{
+}
 
-// entry point of epoch callback in glibc, invoked by wasmtime
+/* entry point of epoch callback in glibc, invoked by wasmtime */
 __attribute__((export_name("signal_callback")))
-void signal_callback(__sighandler_t callback, int signal) {
-// directly call into user's custom signal handler
-if(callback != 0)
-  callback(signal);
+void
+signal_callback (__sighandler_t callback, int signal)
+{
+  /* directly call into user's custom signal handler */
+  if (callback != 0)
+    callback (signal);
 }
 
-// rawposix sigaction struct
-struct rawposix_sigaction {
-__sighandler_t handler;
-unsigned long long sa_mask;
-int sa_flags;
+/* rawposix sigaction struct */
+struct rawposix_sigaction
+{
+  __sighandler_t handler;
+  unsigned long long sa_mask;
+  int sa_flags;
 };
 
 int
 __libc_sigaction (int sig, const struct sigaction *act, struct sigaction *oact)
 {
-// we do the manual translation between glibc sigaction struct and rawposix sigaction struct here
-struct rawposix_sigaction rawposix_act, rawposix_oact;
-// check for NULL pointer
-if (act)
-{
-  rawposix_act.handler = act->sa_handler;
-  rawposix_act.sa_mask = act->sa_mask.__val[0];
-  rawposix_act.sa_flags = act->sa_flags;
-}
-int retval = MAKE_SYSCALL(SIGACTION_SYSCALL, "syscall|sigaction", (uint64_t) sig, (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOST(act ? &rawposix_act : NULL), (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOST(oact ? &rawposix_oact : NULL), NOTUSED, NOTUSED, NOTUSED);
+  /* manual translation between glibc sigaction struct and rawposix sigaction struct */
+  struct rawposix_sigaction rawposix_act, rawposix_oact;
 
-// check for NULL pointer
-if (oact)
-{
-  oact->sa_handler = rawposix_oact.handler;
-  oact->sa_mask.__val[0] = rawposix_oact.sa_mask;
-  oact->sa_flags = rawposix_oact.sa_flags;
-}
+  /* check for NULL pointer */
+  if (act)
+    {
+      rawposix_act.handler = act->sa_handler;
+      rawposix_act.sa_mask = act->sa_mask.__val[0];
+      rawposix_act.sa_flags = act->sa_flags;
+    }
 
-return retval;
+  int retval = MAKE_SYSCALL (SIGACTION_SYSCALL, "syscall|sigaction",
+                             (uint64_t) sig,
+                             (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOST (act ? &rawposix_act : NULL),
+                             (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOST (oact ? &rawposix_oact : NULL),
+                             NOTUSED, NOTUSED, NOTUSED);
+
+  /* check for NULL pointer */
+  if (oact)
+    {
+      oact->sa_handler = rawposix_oact.handler;
+      oact->sa_mask.__val[0] = rawposix_oact.sa_mask;
+      oact->sa_flags = rawposix_oact.sa_flags;
+    }
+
+  return retval;
 }
 libc_hidden_def (__libc_sigaction)
