943ef0daf0c471b0dd5514cef0c0f3eafc03b114
ssannkkallpp <ssannkkallpp@gmail.com>
2025-10-24 02:33:29 +0000

    Minor fix

--- src/glibc/nptl/pthread_create.c
+++ src/glibc/nptl/pthread_create.c
@@ -660,11 +660,10 @@ out:
   
   // signal other threads that the thread has exited
   pd->tid = 0;
-  MAKE_SYSCALL(FUTEX_SYSCALL, "syscall|futex", (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOT(&pd->tid), (uint64_t) FUTEX_WAKE, (uint64_t) 1, (uint64_t)0, 0, (uint64_t)0);
+  MAKE_SYSCALL(FUTEX_SYSCALL, "syscall|futex", (uint64_t) TRANSLATE_GUEST_POINTER_TO_HOST(&pd->tid), (uint64_t) FUTEX_WAKE, (uint64_t) 1, (uint64_t)0, 0, (uint64_t)0);
   while (1)
     // replacing with lind exit
     exit(0);
-
   /* NOTREACHED */
 }
 
