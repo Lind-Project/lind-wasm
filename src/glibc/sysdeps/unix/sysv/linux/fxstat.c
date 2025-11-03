ffad02784137d8d4e816ad90108351efbaef05c5
ssannkkallpp <ssannkkallpp@gmail.com>
2025-11-03 15:49:52 -0500

    Added addr translation for fxstat, shmctl, sigprocmask

--- src/glibc/sysdeps/unix/sysv/linux/fxstat.c
+++ src/glibc/sysdeps/unix/sysv/linux/fxstat.c
@@ -36,7 +36,7 @@ int
 __fxstat (int vers, int fd, struct stat *buf)
 {
   uint64_t host_buf = TRANSLATE_GUEST_POINTER_TO_HOST (buf);
-  return MAKE_SYSCALL (FSTATFS_SYSCALL, "syscall|fxstat", (uint64_t) vers,
+  return MAKE_SYSCALL (FXSTAT_SYSCALL, "syscall|fxstat", (uint64_t) vers,
 		       (uint64_t) fd, host_buf,
 		       NOTUSED, NOTUSED, NOTUSED);
 }
