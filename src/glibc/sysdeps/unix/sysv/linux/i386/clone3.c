cd0c9f14414a58dc4f76c7b2f510789e9e8c461c
ssannkkallpp <ssannkkallpp@gmail.com>
2025-11-05 19:17:28 +0000

    Minor fix to clone3

--- src/glibc/sysdeps/unix/sysv/linux/i386/clone3.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/clone3.c
@@ -3,6 +3,7 @@
 #include <syscall-template.h>
 #include <stdlib.h>
 #include <lind_syscall_num.h>
+#include <addr_translation.h>
 
 int __GI___clone3 (struct clone_args *cl_args, size_t size, int (*func)(void *), void *arg) {
   uint64_t host_cl_args = TRANSLATE_GUEST_POINTER_TO_HOST(cl_args);
