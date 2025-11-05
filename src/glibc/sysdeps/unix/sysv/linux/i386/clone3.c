a29a001cdafd0e1d76c1ee64cc5172cf8fe45091
ssannkkallpp <ssannkkallpp@gmail.com>
2025-11-05 21:05:34 +0000

    Resolve clone3 compile error

--- src/glibc/sysdeps/unix/sysv/linux/i386/clone3.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/clone3.c
@@ -3,18 +3,20 @@
 #include <syscall-template.h>
 #include <stdlib.h>
 #include <string.h>  // For memcpy
-#include <sched.h>  // For struct clone_args
 #include <lind_syscall_num.h>
 #include <addr_translation.h>
 
 int __GI___clone3 (struct clone_args *cl_args, size_t size, int (*func)(void *), void *arg) {
   uint64_t host_cl_args = TRANSLATE_GUEST_POINTER_TO_HOST(cl_args);
-  struct clone_args local_args;
-  memcpy(&local_args, (void *)host_cl_args, sizeof(struct clone_args));
+  void *local_args = alloca(size);
+  memcpy(local_args, (void *)host_cl_args, size);
 
-  local_args.child_tid = TRANSLATE_GUEST_POINTER_TO_HOST((void *)local_args.child_tid);
+  uint64_t *child_tid_ptr = (uint64_t *)((char *)local_args + 16);
+  if (*child_tid_ptr) {
+    *child_tid_ptr = TRANSLATE_GUEST_POINTER_TO_HOST((void *)(*child_tid_ptr));
+  }
 
-  int pid = MAKE_SYSCALL(CLONE_SYSCALL, "syscall|clone3", (uint64_t) &local_args, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
+  int pid = MAKE_SYSCALL(CLONE_SYSCALL, "syscall|clone3", (uint64_t) local_args, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
   if(pid == 0 && func != NULL) {
     int ret = func(arg);
     exit(ret);
