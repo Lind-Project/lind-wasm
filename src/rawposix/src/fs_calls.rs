e48c60808996ff1a3ee30e03179997228b7b4714
drapl0n <87269662+drapl0n@users.noreply.github.com>
2025-11-17 18:31:58 -0500

    Logging: add lind_debug_panic macro (#518)
    * add lind_debug_panic micro
    
    * fix formatting

--- src/rawposix/src/fs_calls.rs
+++ src/rawposix/src/fs_calls.rs
@@ -15,6 +15,7 @@ use sysdefs::constants::fs_const::{
 use sysdefs::constants::lind_platform_const::{FDKIND_KERNEL, MAXFD, UNUSED_ARG, UNUSED_ID};
 use sysdefs::constants::sys_const::{DEFAULT_GID, DEFAULT_UID};
 use sysdefs::constants::LIND_ROOT;
+use sysdefs::logging::lind_debug_panic;
 use typemap::cage_helpers::*;
 use typemap::datatype_conversion::*;
 use typemap::filesystem_helpers::{convert_fstatdata_to_user, convert_statdata_to_user};
@@ -685,7 +686,9 @@ pub fn mmap_syscall(
     }
 
     if prot & PROT_EXEC > 0 {
-        return syscall_error(Errno::EINVAL, "mmap", "PROT_EXEC is not allowed");
+        lind_debug_panic(
+            "Error in syscall: mmap - EINVAL: Invalid Argument: PROT_EXEC is not allowed",
+        );
     }
 
     // check if the provided address is multiple of pages
@@ -3703,7 +3706,9 @@ pub fn shmget_syscall(
     }
 
     if key == IPC_PRIVATE {
-        return syscall_error(Errno::ENOENT, "shmget", "IPC_PRIVATE not implemented");
+        lind_debug_panic(
+            "Error in syscall: shmget - ENOENT: no such file or directory: IPC_PRIVATE not implemented",
+        );
     }
     let shmid: i32;
     let metadata = &SHM_METADATA;
