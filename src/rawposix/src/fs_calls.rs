9607a95219c5dcac6a5f6e5ccaca4e41bd3ec4a2
ssannkkallpp <ssannkkallpp@gmail.com>
2025-11-18 00:47:34 +0000

    Use lind_debug_panic in ioctl (rawposix)

--- src/rawposix/src/fs_calls.rs
+++ src/rawposix/src/fs_calls.rs
@@ -3572,12 +3572,12 @@ pub fn ioctl_syscall(
 
     // Only support FIONBIO and FIOASYNC. Return error for unsupported requests.
     if req_arg != FIONBIO as u64 && req_arg != FIOASYNC as u64 {
-        return syscall_error(Errno::EINVAL, "ioctl", "Unsupported ioctl request");
+        lind_debug_panic("Error in syscall: ioctl - EINVAL: Unsupported ioctl request");
     }
 
     let wrappedvfd = fdtables::translate_virtual_fd(cageid, vfd_arg);
     if wrappedvfd.is_err() {
-        return syscall_error(Errno::EBADF, "ioctl", "Bad File Descriptor");
+        lind_debug_panic("Error in syscall: ioctl - EBADF: Bad File Descriptor");
     }
 
     let vfd = wrappedvfd.unwrap();
