52ff24a60e00d1a53cf292ad6d4d1c911dda18f5
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2025-12-22 21:54:13 -0500

    Fix: copy_data_between_cages address translations

--- src/typemap/src/datatype_conversion.rs
+++ src/typemap/src/datatype_conversion.rs
@@ -289,6 +289,14 @@ pub fn sc_convert_uaddr_to_host(uaddr: u64, addr_cageid: u64, cageid: u64) -> u6
             return 0;
         }
     }
+    
+    let cage = get_cage(addr_cageid).unwrap();
+    let vmmap = cage.vmmap.read();
+    let base_addr = vmmap.base_address.unwrap() as u64;
+
+    if uaddr < base_addr {
+        return uaddr + base_addr;
+    }
 
     uaddr
 }
