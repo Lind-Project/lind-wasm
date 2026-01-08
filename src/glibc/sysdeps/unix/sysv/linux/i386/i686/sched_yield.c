f39e2173c76ec8aefa2aaf89fb3e2ea5a87058e3
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-01-08 22:56:01 +0530

    Fix typo in MAKE LEGACY SYSCALL

--- src/glibc/sysdeps/unix/sysv/linux/i386/i686/sched_yield.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/i686/sched_yield.c
@@ -4,7 +4,7 @@
 
 int __GI___sched_yield (void)
 {
-	return MAKE_LEGACY_SYSCALL(SCHED_YIELD_SYSCALL, "syscall|geteuid", NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED, TRANSLATE_ERRNO_ON);
+	return MAKE_LEGACY_SYSCALL(SCHED_YIELD_SYSCALL, "syscall|sched_yield", NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED, TRANSLATE_ERRNO_ON);
 }
 
 weak_alias(__GI___sched_yield, __sched_yield)
