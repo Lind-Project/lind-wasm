4ff3354440eb0dc98a2c64d260056f92ce27698b
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-01-18 13:48:44 -0500

    Fix address translation for `copy_data_between_cages` (#582)
    * Fix: copy_data_between_cages address translations
    
    * Lint fix: improper blank line
    
    * Do not convert NULL in sc_convert_uaddr_to_host()
    
    * Add comments for cage/memory and glibc address conversions
    
    * Panic instead of a silent error
    
    * Fix lint error.
    
    * Fix lint error.
    
    * Final linting fix

--- src/typemap/src/datatype_conversion.rs
+++ src/typemap/src/datatype_conversion.rs
@@ -290,7 +290,24 @@ pub fn sc_convert_uaddr_to_host(uaddr: u64, addr_cageid: u64, cageid: u64) -> u6
         }
     }
 
-    uaddr
+    // Do not convert on NULL.
+    if uaddr == 0 {
+        return uaddr;
+    }
+
+    let cage = get_cage(addr_cageid).unwrap();
+    let vmmap = cage.vmmap.read();
+    let base_addr = vmmap.base_address.unwrap() as u64;
+
+    // Only convert to host if not already converted.
+    if uaddr >= base_addr {
+        panic!(
+            "sc_convert_uaddr_to_host: invalid uaddr {:#x} - expected a cage-relative address",
+            uaddr
+        );
+    }
+
+    uaddr + base_addr
 }
 
 /// Translates a user-provided address from the Cage's virtual memory into
