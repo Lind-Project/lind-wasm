e786aba563d59a15c48c3f8441967d547109876e
Qianxi Chen <qc815@nyu.edu>
2026-01-27 03:38:10 +0000

    removed printf in test

--- tests/unit-tests/signal_tests/deterministic/signal_int_ignored.c
+++ tests/unit-tests/signal_tests/deterministic/signal_int_ignored.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <assert.h>
 
 int main(void) {
     pid_t child = fork();
@@ -17,37 +18,29 @@ int main(void) {
         pid_t ppid = getppid();
 
         sleep(1);
-        printf("child: sending SIGCHLD to parent (without exiting)\n");
         if (kill(ppid, SIGCHLD) == -1) {
             perror("child kill(SIGCHLD)");
         }
 
         // Keep running a bit so parent is genuinely waiting for our exit.
-        sleep(2);
-        printf("child: now exiting\n");
+        sleep(1);
         _exit(0);
     }
 
     int status;
     for (;;) {
-        printf("parent: calling waitpid() (will block)...\n");
         pid_t r = waitpid(child, &status, 0);
         if (r == -1) {
-            if (errno == EINTR) {
-                printf("parent: waitpid() interrupted by signal (EINTR)\n");
-            }
             perror("waitpid");
+            assert("waitpid interrupted");
             return 1;
         }
 
-        printf("parent: waitpid returned\n");
-        if (WIFEXITED(status)) {
-            printf("parent: child exited with status %d\n", WEXITSTATUS(status));
-        } else if (WIFSIGNALED(status)) {
-            printf("parent: child killed by signal %d\n", WTERMSIG(status));
-        }
+        assert(status == 0);
         break;
     }
+
+    printf("Test Passed\n");
 
     return 0;
 }
