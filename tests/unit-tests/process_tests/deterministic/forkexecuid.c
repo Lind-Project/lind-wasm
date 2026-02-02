56105e3ce91f63347ddf840f5369cb1f9740d55e
rishabhBudhouliya <rb5727@nyu.edu>
2026-02-02 08:34:50 -0500

    modify fork exec to assume 0 gid, uids for test

--- tests/unit-tests/process_tests/deterministic/forkexecuid.c
+++ tests/unit-tests/process_tests/deterministic/forkexecuid.c
@@ -4,25 +4,33 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 int main(void) {
-  pid_t pid;
-  puts("printing getgid:");
-  printf("%d\n", getgid());
-  puts("printing getuid:");
-  printf("%d\n", getuid());
-  puts("printing getegid:");
-  printf("%d\n", getegid());
-  puts("printing geteuid:");
-  printf("%d\n", geteuid());
+  assert(getgid() == 0 && "parent gid should be 0");                                                                                                                       
+  assert(getuid() == 0 && "parent uid should be 0");                                                                                                                       
+  assert(getegid() == 0 && "parent egid should be 0");                                                                                                                     
+  assert(geteuid() == 0 && "parent euid should be 0"); 
   
-  pid = fork();	
-  if (pid == -1) {
-    perror("fork error");
+  pid_t pid = fork();                                                                                                                                                      
+  assert(pid != -1 && "fork should succeed");
+
+  if (pid == 0) {                                                                                                                                                          
+    // Child asserts IDs preserved after fork                                                                                                                              
+    assert(getgid() == 0 && "child gid should be 0 after fork");                                                                                                           
+    assert(getuid() == 0 && "child uid should be 0 after fork");                                                                                                           
+    assert(getegid() == 0 && "child egid should be 0 after fork");                                                                                                         
+    assert(geteuid() == 0 && "child euid should be 0 after fork");                                                                                                         
+                                                                                                                                                                                                                                                                                                        
+    char* arr[] = {"getuid", NULL};                                                                                                                                        
+    execv("automated_tests/getuid", arr);                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                          
+    assert(0 && "execv should not return");                                                                                                                                
   }
-  else if (pid == 0) {
-    char* arr[] = {"getuid", NULL};
-    execv("automated_tests/getuid", arr);
-  }
-  wait(NULL);
+  int status;                                                                                                                                                              
+  wait(&status);                                                                                                                                                           
+  assert(WIFEXITED(status) && "child should exit normally");                                                                                                               
+  assert(WEXITSTATUS(status) == 0 && "child should exit with status 0");
+
+  return 0;
 }
