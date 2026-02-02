431df631f843165636bed147534724fc850321c6
rishabhBudhouliya <rb5727@nyu.edu>
2026-02-02 12:51:25 -0500

    refactor test with macro and better comments

--- tests/unit-tests/process_tests/deterministic/forkexecuid.c
+++ tests/unit-tests/process_tests/deterministic/forkexecuid.c
@@ -6,21 +6,26 @@
 #include <sys/wait.h>
 #include <assert.h>
 
+// NOTE: This test assumes the test environment runs as root (UID/GID = 0).                                                                                                
+// If tests run as non-root, these assertions will fail even if fork/exec work correctly.                                                                                  
+#define ROOT_UID 0                                                                                                                                                         
+#define ROOT_GID 0 
+
 int main(void) {
-  assert(getgid() == 0 && "parent gid should be 0");                                                                                                                       
-  assert(getuid() == 0 && "parent uid should be 0");                                                                                                                       
-  assert(getegid() == 0 && "parent egid should be 0");                                                                                                                     
-  assert(geteuid() == 0 && "parent euid should be 0"); 
+  assert(getgid() == ROOT_GID && "parent gid should be 0");                                                                                                                       
+  assert(getuid() == ROOT_UID && "parent uid should be 0");                                                                                                                       
+  assert(getegid() == ROOT_GID && "parent egid should be 0");                                                                                                                     
+  assert(geteuid() == ROOT_UID && "parent euid should be 0"); 
   
   pid_t pid = fork();                                                                                                                                                      
   assert(pid != -1 && "fork should succeed");
 
   if (pid == 0) {                                                                                                                                                          
     // Child asserts IDs preserved after fork                                                                                                                              
-    assert(getgid() == 0 && "child gid should be 0 after fork");                                                                                                           
-    assert(getuid() == 0 && "child uid should be 0 after fork");                                                                                                           
-    assert(getegid() == 0 && "child egid should be 0 after fork");                                                                                                         
-    assert(geteuid() == 0 && "child euid should be 0 after fork");                                                                                                         
+    assert(getgid() == ROOT_GID && "child gid should be 0 after fork");                                                                                                           
+    assert(getuid() == ROOT_UID && "child uid should be 0 after fork");                                                                                                           
+    assert(getegid() == ROOT_GID && "child egid should be 0 after fork");                                                                                                         
+    assert(geteuid() == ROOT_UID && "child euid should be 0 after fork");                                                                                                         
                                                                                                                                                                                                                                                                                                         
     char* arr[] = {"getuid", NULL};                                                                                                                                        
     execv("automated_tests/getuid", arr);                                                                                                                                  
