173e2a2a90f24dc1e9007b091e0ae5cf53e5533e
Vidya Lakshmi Rajagopalan <vidyalakshmir@users.noreply.github.com>
2026-02-17 12:33:53 -0500

    Glibc static build - WIP (#669)
    * glibc: Use Implies file to select IEEE-754 math over i386 FPU sysdeps
    
    * Updated glibc build script to create static archives (.a) files for libc, libm and other glibc modules
    
    * Update the lind_compile script to link against libm by adding -lm option
    
    * Enhance comments and structure in make_archive.sh
    
    Refactor make_archive.sh to improve comments and structure.
    
    * Update glibc build script
    
    Removed the creation of the libpthread.a archive and its success check.
    
    * Updated SCRIPT_DIR to the beginning of the script
    
    * Fix undefined symbol error for sched_setscheduler and sched_get_priority_max
    
    * Using default stub implementations of Floating point rounding and exception handling from math/ folder
    
    * Using default stub implementations within math/ folder of fegetround and fesetround instead of i386 specific implementation containing assembly
    
    * Fix undefined symbol error for sched_get_priority_min and sched_getscheduler by adding weak symbols within arch specific impl
    
    * Add math tests
    
    * Updated math of math tests

--- src/glibc/sysdeps/unix/sysv/linux/i386/i686/sched_primax.c
+++ src/glibc/sysdeps/unix/sysv/linux/i386/i686/sched_primax.c
@@ -6,4 +6,5 @@ __GI___sched_get_priority_max (int __algorithm)
   return 0;
 }
 
+weak_alias(__GI___sched_get_priority_max, sched_get_priority_max)
 weak_alias(__GI___sched_get_priority_max, __sched_get_priority_max)
