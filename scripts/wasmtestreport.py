f815ba067b33c1e9880279e15f6ff4a1fd3dbe71
MHemmings <discount.yoyos@gmail.com>
2026-02-18 08:09:50 -0800

    Correct output of debug info (#753)

--- scripts/wasmtestreport.py
+++ scripts/wasmtestreport.py
@@ -521,15 +521,10 @@ def run_compiled_wasm(wasm_file, timeout_sec=DEFAULT_TIMEOUT):
     try:
         run_start = time.perf_counter()
         proc = run_subprocess(run_cmd,label="wasm run",timeout=timeout_sec, cwd=None, shell = False)
-        run_time = round(time.perf_counter() - run_start, 6)
-        full_output = proc.stdout + proc.stderr
-        
-        #removing the first line in output as it is the command being run by the bash script
-        lines = full_output.splitlines()
-        filtered_lines = lines[1:]
-        filtered_output = "\n".join(filtered_lines)
+        run_time = round(time.perf_counter() - run_start, 6)       
+        output = proc.stdout if proc.returncode == 0 else (proc.stdout + proc.stderr)
 
-        return (proc.returncode, full_output, run_time)
+        return (proc.returncode, output, run_time)
 
     except subprocess.TimeoutExpired as e:
         return ("timeout", f"Timed Out (timeout: {timeout_sec}s)", None)
