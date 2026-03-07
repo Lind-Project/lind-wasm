2177982903b387b6cdcc807686ddf8329b4112d5
Alice W <wenyaxuan0925@outlook.com>
2026-03-06 19:29:34 -0500

    Add strace code

--- tests/grate-tests/strace/strace_grate.c
+++ tests/grate-tests/strace/strace_grate.c
@@ -27,6 +27,12 @@ void dump_trace(void) {
     }
 }
 
+__attribute__((destructor))
+static void dump_trace_at_exit(void) {
+    fprintf(stderr, "[Grate] destructor: trace_idx=%d\n", trace_idx);
+    dump_trace();
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <cage_binary> [args...]\n", argv[0]);
@@ -41,11 +47,10 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         int cageid = getpid();
-        // loop to register syscall handlers
         for (int i = 0; i < MAX_SYSCALLS; i++) {
             if (syscall_handler_table[i] != NULL) {
                 uint64_t fn_ptr = (uint64_t)(uintptr_t)syscall_handler_table[i];
-                register_handler(cageid, i, grateid,  fn_ptr);
+                register_handler(cageid, i, grateid, fn_ptr);
             }
         }
         if (execv(argv[1], &argv[1]) == -1) {
@@ -55,10 +60,9 @@ int main(int argc, char *argv[]) {
     }
 
     int status;
-    
     while (wait(&status) > 0) {
         fprintf(stderr, "[Grate] process terminated, status: %d\n", status);
     }
-    dump_trace();
+
     return 0;
 }
