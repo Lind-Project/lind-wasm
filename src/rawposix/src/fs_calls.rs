19530ced5e494cd52685fb3f1d8029c760610da7
rishabhBudhouliya <rb5727@nyu.edu>
2026-03-09 00:48:00 +0000

    revert panic behavior for unsupported flags

--- src/rawposix/src/fs_calls.rs
+++ src/rawposix/src/fs_calls.rs
@@ -810,9 +810,8 @@ pub extern "C" fn mmap_syscall(
         | MAP_STACK as i32
         | MAP_DENYWRITE as i32;
     if flags & !allowed_flags != 0 {
-          eprintln!("mmap: stripping unknown flags 0x{:x} (original flags=0x{:x})", flags & !allowed_flags, flags);
-          flags = flags & allowed_flags;
-      }
+        lind_debug_panic("Unsupported mmap flag detected! Only MAP_FIXED, MAP_SHARED, MAP_PRIVATE, MAP_POPULATE AND MAP_ANONYMOUS allowed");
+    }
 
     if prot & PROT_EXEC > 0 {
         lind_debug_panic("mmap protection flag PROT_EXEC is not allowed in Lind");
