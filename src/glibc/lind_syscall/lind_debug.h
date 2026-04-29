a7f97c6cf2559a13b8c957fd9f7de4034275afc3
Daniel Zhao <zz10239@nyu.edu>
2026-04-29 18:33:25 +0000

    Restore lind debug import helper

--- src/glibc/lind_syscall/lind_debug.h
+++ src/glibc/lind_syscall/lind_debug.h
@@ -5,6 +5,14 @@
 void lind_debug_panic (const char* msg);
 
 #ifdef LIND_DEBUG
+// lind_debug raw WASM imports
+unsigned int __lind_debug_num(unsigned int num);
+const char* __lind_debug_str(const char *str);
+
+// lind_debug force import
+void __lind_debug_import(void);
+
+// formatted debug print helper
 void lind_debug_printf(const char *fmt, ...);
 #endif // LIND_DEBUG
 
