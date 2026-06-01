91e2b5c7e42d802b7862626f49400dec50ae2e73
Alice W <wenyaxuan0925@outlook.com>
2026-06-01 13:23:50 -0400

    Add compatibility of renameat

--- src/rawposix/src/fs_calls.rs
+++ src/rawposix/src/fs_calls.rs
@@ -2585,28 +2585,23 @@ fn renameat_inner(
     };
 
     let ret = unsafe {
-        // if use_renameat2 {
-        //     libc::renameat2(
-        //         old_kernel_fd,
-        //         c_oldpath.as_ptr(),
-        //         new_kernel_fd,
-        //         c_newpath.as_ptr(),
-        //         flags,
-        //     )
-        // } else {
-        //     libc::renameat(
-        //         old_kernel_fd,
-        //         c_oldpath.as_ptr(),
-        //         new_kernel_fd,
-        //         c_newpath.as_ptr(),
-        //     )
-        // }
-        libc::renameat(
+        if use_renameat2 {
+            libc::syscall(
+            libc::SYS_renameat2,
             old_kernel_fd,
             c_oldpath.as_ptr(),
             new_kernel_fd,
             c_newpath.as_ptr(),
-        )
+            flags,
+        ) as libc::c_int
+        } else {
+            libc::renameat(
+                old_kernel_fd,
+                c_oldpath.as_ptr(),
+                new_kernel_fd,
+                c_newpath.as_ptr(),
+            )
+        }
     };
     if ret < 0 {
         let errno = get_errno();
